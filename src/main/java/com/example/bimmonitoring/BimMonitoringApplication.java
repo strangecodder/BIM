93c964c816a911c0f8780d12dce0c48c7aef78ae
strangecodder <strangecodder@gmail.com>
2023-11-26 18:02:59 +0300

    first

---
+++ src/main/java/com/example/bimmonitoring/BimMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.bimmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimMonitoringApplication.class, args);
+	}
+
+}

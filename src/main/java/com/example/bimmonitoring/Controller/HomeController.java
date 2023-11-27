2b37f6055e2fa6031e9e408301cc0b4e25eef670
ArtHouse17 <102299192+ArtHouse17@users.noreply.github.com>
2023-11-27 09:54:50 +0300

    0.3
    Added working header

--- src/main/java/com/example/bimmonitoring/Controller/HomeController.java
+++ src/main/java/com/example/bimmonitoring/Controller/HomeController.java
@@ -1,4 +1,4 @@
-package Controller;
+package com.example.bimmonitoring.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String getHome(){
-        return "home";
+        return "Desktop1";
     }
 }
